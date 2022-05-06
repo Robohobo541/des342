984f4032d9e89a58fd99632ccec836220941b6c7
Robohobo541 <97709342+Robohobo541@users.noreply.github.com>
2022-05-05 22:17:57 -0700

    HW 2 done

---
+++ homework/homework 2/script.js
@@ -0,0 +1,74 @@
+// $('body').css('background','pink')
+
+// $('h1').click(
+//     function() {
+//         $('body').toggleClass('purple')
+//     }
+// )
+
+// let rainbow = 0
+
+// let colorVal = `hsl(${rainbow},100,100)'
+
+// $('.button').click
+// (
+//     function(){
+//         rainbow+30
+//         $('body').css('background',`${colorVal}`)
+//     }
+// )
+
+$('.one') .click(
+    function(){
+        $('.cells').attr('src','img/rc1.png')
+    }
+)
+
+$('.two') .click(
+    function(){
+        $('.cells').attr('src','img/rc2.png')
+    }
+)
+
+$('.three') .click(
+    function(){
+        $('.cells').attr('src','img/rc3.png')
+    }
+)
+
+$('.four') .click(
+    function(){
+        $('.cells').attr('src','img/rc4.png')
+    }
+)
+
+$('.five') .click(
+    function(){
+        $('.cells').attr('src','img/rc5.png')
+    }
+)
+
+$('.six') .click(
+    function(){
+        $('.cells').attr('src','img/rc6.png')
+    }
+)
+
+$('.seven') .click(
+    function(){
+        $('.cells').attr('src','img/rc7.png')
+    }
+)
+
+$('.eight') .click(
+    function(){
+        $('.cells').attr('src','img/rc8.png')
+    }
+)
+
+$('.nine') .click(
+    function(){
+        $('.cells').attr('src','img/rc9.png')
+    }
+)
+
