97b992b07dda8c23b3bfda2bab2be352c600e67d
Robohobo541 <97709342+Robohobo541@users.noreply.github.com>
2022-05-24 12:28:45 -0700

    js stuff

--- project1/script.js
+++ project1/script.js
@@ -3,9 +3,43 @@ $('#wake-up-btn').click(function () {
 
   setTimeout(function () {
     $('#wake-up-img').attr('src', '../img/wake3.gif')
-  }, 3000)
+  }, 7000)
   setTimeout(function () {
-    $('#wake-up-img').attr('src', '../img/wake5.gif')
+    $('#wake-up-img').attr('src', '../img/wake4.gif')
     $('#scroll-btn').css('display', 'block')
-  }, 2000)
+    $('#work-btn').css('display', 'block')
+  },12000)
+})
+
+$('#scroll-btn').click(function () {
+  $('#wake-up-img').attr('src', '../img/wake5.gif')
+  setTimeout(function () {
+    $('#wake-up-img').attr('src', '../img/wake4.gif')
+  }, 3000)
+})
+
+$('#red-btn').click(function () {
+  $('#work-img').attr('src', '../img/work-big-red.gif')
+
+  setTimeout(function () {
+    $('#work-img').attr('src', '../img/work-idle.gif')
+  }, 3000)  
+})
+
+$('#lever-btn').click(function () {
+  $('#work-img').attr('src', '../img/work-lever.gif')
+
+  setTimeout(function () {
+    $('#work-img').attr('src', '../img/work-idle.gif')
+  }, 3500)  
+})
+
+$('#game-left-btn').click(function () {
+  $('#game-img').attr('src', '../img/game-left.gif')
+
+})
+
+$('#game-right-btn').click(function () {
+  $('#game-img').attr('src', '../img/game-right.gif')
+
 })
